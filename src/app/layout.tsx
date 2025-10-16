7c52f1aa030a66f4e2f3c8880476b9878f2a5b7f
Niladri <niladrisengit12@gmail.com>
2025-10-16 11:03:33 +0530

    fix: correct formatting in layout.tsx import statement

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,7 +2,7 @@ import { AuthProvider } from "@/providers/auth.provider";
 import { ThemeProvider } from "@/providers/theme.provider";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css"; 
+import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
