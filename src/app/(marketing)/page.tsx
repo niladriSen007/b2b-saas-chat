bf756a9c9386555371b7ddeeff65c3444495e9b2
Niladri <niladrisengit12@gmail.com>
2025-10-16 09:53:48 +0530

    feat: add HeroHeader and HeroSection components for marketing page

---
+++ src/app/(marketing)/page.tsx
@@ -0,0 +1,12 @@
+import { HeroHeader } from "@/app/(marketing)/_components/header"
+import HeroSection from "@/app/(marketing)/_components/hero-section"
+
+const page = () => {
+  return (
+    <>
+      <HeroHeader />
+      <HeroSection />
+    </>
+  )
+}
+export default page
